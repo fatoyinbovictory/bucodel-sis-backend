e70483e4b584bfe95fd97f253c54d9d24ef86548
Victory Fatoyinbo <vicfatoyinbo@gmail.com>
2023-03-09 13:51:23 +0100

    changes'

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 mongoose.connect(
   "mongodb://localhost/bucodelSis",
@@ -7,11 +8,18 @@ mongoose.connect(
     console.log("Connected to database");
   },
   (e) => {
-    console.log("Failed to connected due to", e);
+    console.log(`Failed to connected due to ${e}`);
   }
 );
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("api/register", (req, res) => {
+  console.log(req.body);
+});
 
 const PORT = process.env.PORT || 5000;
 
