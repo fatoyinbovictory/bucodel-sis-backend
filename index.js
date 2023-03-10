8e50104c237aa1f6be7e3ddde76491a28ba7b0ef
Victory Fatoyinbo <vicfatoyinbo@gmail.com>
2023-03-10 13:32:36 +0100

    changes

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+// imports
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const generalRoutes = require("./routes/generalRoutes");
+const studentRoutes = require("./routes/studentRoutes");
 
-mongoose.connect(
-  "mongodb://localhost/bucodelSis",
-  () => {
-    console.log("Connected to database");
-  },
-  (e) => {
-    console.log(`Failed to connected due to ${e}`);
-  }
-);
-
+// express app
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-app.post("api/register", (req, res) => {
-  console.log(req.body);
-});
+//routes
+app.use("/api", generalRoutes);
+app.use("/api/student", studentRoutes);
+// app.post("api/register", (req, res) => {
+//   console.log(req.body);
+// });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+// connect to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  // .then(() => {
+  //   console.log("Cnnecting to database...");
+  // })
+  .then(() => {
+    console.log("Successfully Connected to Database");
+    app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+  })
+  .catch((e) => {
+    console.log(`Unable to connect to database due to ${e}`);
+  });
